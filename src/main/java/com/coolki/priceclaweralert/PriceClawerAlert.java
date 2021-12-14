650680ed88d275e4027e1b2be657183fb26c1164
Travis Li <coolki@gmail.com>
2021-12-14 19:23:04 +0800

    Update PriceClawerAlert.java
    test

--- src/main/java/com/coolki/priceclaweralert/PriceClawerAlert.java
+++ src/main/java/com/coolki/priceclaweralert/PriceClawerAlert.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PriceClawerAlert {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PriceClawerAlert.class, args);
 	}
